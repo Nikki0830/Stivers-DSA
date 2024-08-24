cce0ccb681be4cd592d377a3cd04faf3781da587
Nikki0830 <gniki0830@gmail.com>
2024-08-24 13:35:02 +0530

    Pattern Problems

---
+++ Patterns/p4.js
@@ -0,0 +1,15 @@
+//1
+//2 2
+//3 3 3
+//4 4 4 4
+//5 5 5 5 5
+
+//here we are printing rows nos in each line
+
+for(let i =1; i <= 5; i++){
+    let spa = ""
+    for(let j = 1; j <= i; j++){
+        spa += i + " "
+    }
+    console.log(spa)
+}
